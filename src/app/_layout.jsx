9950fda877fbb0d1b884fea173097cf3ecea9ff1
VumenAirlines <shadowdark042@gmail.com>
2025-03-28 10:07:38 +0100

    fixed formatting

--- src/app/_layout.jsx
+++ src/app/_layout.jsx
@@ -1,16 +1,15 @@
 import "src/bootstrapping"
 
-import { Stack } from "expo-router";
+import { Stack } from "expo-router"
 
 export default function RootLayout() {
   return (
     <Stack
-      
       screenOptions={{
         title: "Blood Donor App",
       }}
-    >       
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    >
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
-  );
+  )
 }
