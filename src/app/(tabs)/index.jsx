b58af5ff7645499163c2bc40ac1ffb3da14aba63
Sreenija Veladri <srveladri3035@gmail.com>
2025-04-02 14:06:33 +0200

    Taken from dev

--- src/app/(tabs)/index.jsx
+++ src/app/(tabs)/index.jsx
@@ -1,8 +1,9 @@
 import { observer } from "mobx-react-lite"
 import { Request } from "src/presenters/requestPresenter"
 
-import { reactiveModel } from "src/app/bootstrapping"
+//import { reactiveModel } from "src/app/bootstrapping"
+import { model } from "src/app/model"
 
 export default observer(function IndexPage() {
-  return <Request model={reactiveModel} />
+  return <Request model={model} />
 })
