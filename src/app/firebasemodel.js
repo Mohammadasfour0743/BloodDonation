bd3149d28a16d745e2a56cc91b25175f093756a5
Shubhangi Goswami <sgoswami@kth.se>
2025-04-03 11:42:23 +0200

    updated firestoreModel

--- src/app/firebasemodel.js
+++ src/app/firebasemodel.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app"
+/* import { initializeApp } from "firebase/app"
 import {
   collection,
   doc,
@@ -114,7 +114,7 @@ export function connectToPersistence(model, watchFunction) {
     })
     snapshot.docChanges().forEach((change) => {
       const data = change.doc.data()
-      if (change.type === "removed" || data.current === false) { 
+      if  (change.type === "removed" || data.current === false) { 
         fetchedRequests.push({ id: change.doc.id, ...data, removed: true })
       } else {
         fetchedRequests.push({ id: change.doc.id, ...data, removed: false })
@@ -146,7 +146,7 @@ export function connectToPersistence(model, watchFunction) {
       }
     })
     //console.log("no need for reload", fetchedRequests);
-  })
+  });
 }
 
 //test function for the getDocs methods for requests
@@ -179,3 +179,160 @@ export function connectToPersistence(model, watchFunction) {
 //     console.error("Error saving request:", error);
 //   }
 // }
+ */
+
+import { initializeApp } from "firebase/app"
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  onSnapshot,
+  setDoc,
+  query,
+  where
+} from "firebase/firestore"
+import { runInAction } from "mobx"
+
+import { firebaseConfig } from "./firebaseconfig.js"
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const COLLECTION1 = "donors"
+const COLLECTION2 = "requests"
+
+// making them available at the console to test
+global.doc = doc
+global.setDoc = setDoc
+global.app = db
+
+const docToStore = doc(db, COLLECTION1, "data")
+
+export function connectToPersistence(model, watchFunction) {
+  function modelState() {
+    return [model.user.username, model.user.name, model.user.bloodtype]
+  }
+
+  // persisting to the db
+  function persistModel() {
+    setDoc(
+      docToStore,
+      {
+        name: model.user.name,
+        username: model.user.username,
+        bloodtype: model.user.bloodtype,
+      },
+      { merge: true },
+    )
+  }
+
+  watchFunction(modelState, persistModel)
+
+  getDoc(docToStore)
+    .then((snapshot) => {
+      //console.log("Firestore snapshot retrieved:", snapshot);
+      const data = snapshot.data()
+      //console.log("Data from Firestore:", data);
+
+      // if data exists, updates the model's properties/fields with the stored values
+      // if any field is missing, it updates to the default values (given in the model)
+      if (data) {
+        model.user.name = data.name ?? "default"
+        model.user.username = data.username ?? "default"
+        model.user.bloodtype = data.bloodtype ?? "default"
+      }
+      // if no document during the read, sets the model's properties to the default values.
+      else {
+        model.user.name = "default"
+        model.user.username = "default"
+      }
+    })
+    .catch((error) => console.error("Error reading document:", error))
+
+  // METHOD FOR REQUESTS - Only fetch documents where current is true
+  const requestsQuery = query(
+    collection(db, COLLECTION2),
+    where("current", "==", true)
+  )
+
+  getDocs(requestsQuery)
+    .then((snapshot) => {
+      const fetchedRequests = []
+
+      snapshot.forEach((doc) => {
+        const data = doc.data()
+        fetchedRequests.push({ id: doc.id, ...data })
+      })
+
+      if (fetchedRequests.length > 0) {
+        fetchedRequests.forEach((newRequest) => {
+          const existingRequest = model
+            .getRequests()
+            .find((req) => req.id === newRequest.id)
+
+          if (existingRequest) {
+            const updatedFields = {}
+            Object.keys(newRequest).forEach((key) => {
+              if (existingRequest[key] !== newRequest[key]) {
+                updatedFields[key] = newRequest[key]
+              }
+            })
+
+            if (Object.keys(updatedFields).length > 0) {
+              model.updateRequests(newRequest.id, updatedFields)
+            }
+          } else {
+            model.addRequest(newRequest)
+          }
+        })
+      }
+    })
+    .catch((error) => console.error("error reading the docs:", error))
+
+  // Listen for real-time updates on the filtered query
+  onSnapshot(requestsQuery, (snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      const data = change.doc.data()
+      const id = change.doc.id
+      
+      if (change.type === "added" || change.type === "modified") {
+        const newRequest = { id, ...data }
+        const existingRequest = model
+          .getRequests()
+          .find((req) => req.id === id)
+
+        if (existingRequest) {
+          const updatedFields = {}
+          Object.keys(newRequest).forEach((key) => {
+            if (existingRequest[key] !== newRequest[key]) {
+              updatedFields[key] = newRequest[key]
+            }
+          })
+
+          if (Object.keys(updatedFields).length > 0) {
+            model.updateRequests(id, updatedFields)
+          }
+        } else {
+          model.addRequest(newRequest)
+        }
+      } else if (change.type === "removed") {
+        model.removeRequest(id)
+      }
+    })
+  })
+
+  // Listen for documents where current changes to false
+  const allRequestsQuery = collection(db, COLLECTION2)
+  onSnapshot(allRequestsQuery, (snapshot) => {
+    snapshot.docChanges().forEach((change) => {
+      const data = change.doc.data()
+      const id = change.doc.id
+      
+      // If document is updated to have current=false, remove it from our model
+      if (change.type === "modified" && data.current === false) {
+        model.removeRequest(id)
+      }
+    })
+  })
+}
